f69e57a3e46c9d2187f673443fbf2a79bc11c201
guibedan <guilhermebedan123@gmail.com>
2022-11-04 22:15:19 -0300

    Initial Commit

---
+++ Calculator-App/Calculator_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_AppApp.swift
+//  Calculator-App
+//
+//  Created by Guilherme Bedan on 04/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
